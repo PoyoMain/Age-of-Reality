226176604d279eadf69274094d2ae0a8e55da596
Myriad21 <tr4juansmith@gmail.com>
2024-03-16 20:46:34 -0400

    EnemyPatrol/Chase
    EnemyPatrol/Chase

---
+++ Assets/Scripts/Overworld/Chase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    public float speed;
+    public float detectionRange = 5f;
+    public float chaseDuration = 5f;
+
+    private Transform target;
+    private bool isChasing = false;
+    private float chaseTimer = 0f;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, target.position);
+
+        // Check if the player is within the detection range and start chasing
+        if (distanceToPlayer <= detectionRange)
+        {
+            isChasing = true;
+            chaseTimer = 0f;
+        }
+
+        // If chasing, move towards the player
+        if (isChasing)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            // Increment timer
+            chaseTimer += Time.deltaTime;
+
+            // If chase duration has exceeded, stop chasing
+            if (chaseTimer >= chaseDuration)
+            {
+                isChasing = false;
+            }
+        }
+    }
+}
